2a7610eb832e287f0bbb1c069e91e831848b99b5
Wendel Filho <wfilho@pop-os.localdomain>
2020-04-08 11:32:22 -0300

    PWA

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/blog'
+  get 'pages/courses'
+  get 'pages/bootcamp'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+end
